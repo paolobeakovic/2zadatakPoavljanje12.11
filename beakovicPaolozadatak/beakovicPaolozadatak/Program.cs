74c5c08994f8e2db89751bc1e8894c3c6a8c9e38
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-11-17 10:10:31 +0100

    Add files via upload

---
+++ beakovicPaolozadatak/beakovicPaolozadatak/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beakovicPaolozadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upiši niz : ");
+            string niz = Console.ReadLine();
+
+            Console.WriteLine(niz.Replace(" ", "_"));
+            Console.ReadKey();
+
+        }
+    }
+}
